9ca41630e40d663f338ec4419295250257cd9fbf
Jaime André <jaimeaugusto11@gmail.com>
2025-02-11 01:30:11 +0100

    Novas alterações

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,9 +1,9 @@
-
+export const dynamic = "force-dynamic";
 
 import { createRouteHandler } from "uploadthing/next";
 import { ourFileRouter } from "./core";
 
-
+// Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
 });
